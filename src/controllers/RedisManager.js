bfcb5d056df5a4f8d3deafbcad737ee5a2b4d5e6
Hneumont <hberumengovea@student.neumont.edu>
2025-11-24 19:54:07 -0700

    basic setup

--- src/controllers/RedisManager.js
+++ src/controllers/RedisManager.js
@@ -6,9 +6,38 @@ import { createClient } from "redis";
 //  url: 'redis://USERNAME:PASSWORD:127.0.0.1:6379'
 //})
 const redisClient = createClient({});
+const mT = Object.freeze({ // is this even going to be used?
+  TEXT: "text",
+  GIF: "gif",
+  PNG: "png"
+  //etc etc
+})
 
 redisClient.on("error", (err) => console.log("REDIS Failed ", err));
 
-await redisClient.connnect();
+await redisClient.connect();
 
-// There is currently no meaningful connection to redis
+console.log("Connected to redis")
+createMessage(2,148, "I'll give it to someone special")
+console.log("TEST FINAL")
+//let getNewMsg = await redisClient.get("2:148")
+//console.log(getNewMsg.toString())
+
+
+
+async function createMessage(ConversationID, userID, messageContent){
+  let msgID = ConversationID.toString()+"-"+userID.toString()
+  let newmsg = await redisClient.rPush(msgID,messageContent)
+  console.log("GFIDSAFO")
+  for (let i = 0; i < parseInt(newmsg.toString()); i++){
+    console.log(newmsg[i])
+  }
+  let reteP = await redisClient.lRange(msgID, 0, -1);
+  console.log(reteP)
+  console.log(newmsg.length)
+  console.log("FINISHED")
+}
+
+async function deleteMessage(messageID){
+
+}
