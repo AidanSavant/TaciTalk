372c7be016275341cac7c782ac09d6b4d3197f3c
Hneumont <hberumengovea@student.neumont.edu>
2025-12-02 18:45:14 -0700

    slight adjustments, addition of send/receive

--- src/controllers/SocketController.js
+++ src/controllers/SocketController.js
@@ -5,6 +5,22 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {})
 
-io.on("connection", (socket) => {
+io.on("connection", (socket) => { //function called when user connects to the server
+  if (socket.recovered){
+    console.log("Data recovered Succesfuly")
+    //conversations resolved, no data lost
+  } else{
+    console.log("Data lost!")
+    //unrecovered sockets will lose connection to their conversations
+  }
+});
 
-})
+httpServer.listen(3000); //port might need to be changed to 5050
+
+io.on("message_sent", (message) => { //funciton called when user recieves a message
+
+});
+
+async function sendTo(conversationID, message){
+  io.to(conversationID).emit("message_sent", message)
+}
