527c2ce9f44e86562d596cbcba06a126d6aac122
Hneumont <hberumengovea@student.neumont.edu>
2025-12-03 18:37:06 -0700

    quick adjustments to redis

--- src/controllers/RedisManager.js
+++ src/controllers/RedisManager.js
@@ -19,8 +19,18 @@ await redisClient.connect();
 
 console.log("Connected to redis");
 
+await createMessage(2,15,"This is a message from user 2")
+await createMessage(3,12,"This is a message from a different chat 3")
+await createMessage(3,15,"This should be counted")
+await createMessage(3,15,"This should be counted too")
+await createMessage(3,15,"This should be counted as well")
+await createMessage(3,15,"This should be counted also")
+console.log("TEST PRINT ALL "+await redisClient.keys('*'))
+console.log("total keys: " + (await getUserMessageCount(3, 15)));
+await redisClient.FLUSHALL()
+
 function getMsgID(userID,conversationID){
-  return conversationID.toString() + "-" + userID.toString();
+  return userID.toString() + "-" + conversationID.toString();
 }
 
 async function createMessage(userID, conversationID, messageContent){
@@ -46,8 +56,12 @@ async function getMessageFromIndex(userID,conversationID,index){
 
 async function getUserMessageCount(userID,conversationID){
   let msgID = getMsgID(userID,conversationID);
-  let messages = await redisClient.lLen(msgID);
-  return messages;
+  //let messages = await redisClient.lLen(msgID);
+  let findKey = userID+"-*"
+  console.log(findKey)
+  //let msgCount = await redisClient.KEYS(userID+"-*").length
+  let msgCount = (await redisClient.scan('0', {MATCH: findKey})).length
+  return msgCount;
 }
 
 async function editMessage(userID,conversationID,index,messageContent){
--- src/controllers/SocketController.js
+++ src/controllers/SocketController.js
@@ -1,5 +1,9 @@
-const { Server } = require("socket.io");
-import { app } from '../../server.js';
+//const { Server } = require("socket.io");
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import pkg from '../../server.js'
+const app = pkg.app;
+//import { app } from '../.././server.js';
 const httpServer = createServer(app);
 
 
@@ -17,10 +21,28 @@ io.on("connection", (socket) => { //function called when user connects to the se
 
 httpServer.listen(3000); //port might need to be changed to 5050
 
-io.on("message_sent", (message) => { //funciton called when user recieves a message
+io.engine.on("connection_error", (err) =>{
+  console.log("ERROR: "+err.code+";\n"+err.message+";\n"+err.context)
+})
+
+io.on("message_sent", (userID, messageID, messageType, messageContent) => { //funciton called when user recieves a message
 
 });
 
-async function sendTo(conversationID, message){
-  io.to(conversationID).emit("message_sent", message)
+sendTo();
+
+async function sendTo(conversationID, messageID){
+  let timeSent = new Date().toISOString().slice(0,19).replace('T', ' ');
+  // timesent formatted at [YYYY/MM/DD HH:MM:SS] (UTC TIME)
+  io.to(conversationID).emit("message_sent", userID, messageID, messageType, messageContent)
 }
+
+/*
+MessageID
+timeSent
+MessageType
+MessageContent
+SavedAt
+ConversationID
+UserID
+*/
